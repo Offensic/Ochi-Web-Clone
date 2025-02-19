9a5582f74f6b9cc54fe95a0967bde5ab7b05cfef
Offensic <ziakhan0537@gmail.com>
2025-02-19 01:16:56 -0800

    Fixec Ui chnages on landinga page

--- src/Component/Home Component/landingpage.jsx
+++ src/Component/Home Component/landingpage.jsx
@@ -8,8 +8,8 @@ function landingpage() {
     return (
         <div data-scroll data-scroll-section data-scroll-speed="-.3" className='relative w-full h-full   pt-32 ' >
 
-            <div className='w-[250px] text-white bg-[black] p-4 z-[999] fixed  right-5 bottom-3 -translate-y-1/2 -right    rounded-md '>
-                <p>Note: 👇</p>
+            <div className='w-[250px] text-white bg-[black] p-4 z-[999] fixed right-5 bottom-10 rounded-md '>
+                {/* <p>Note: 👇</p> */}
                 <p className='text-sm tracking-wide '>This site is a cloned project for showcasing my skills.  <a href="https://ochi.design/" target='_blank' className='cursor-pointer hover:text-orange-300'>Visit the original site </a></p>
             </div>
 
